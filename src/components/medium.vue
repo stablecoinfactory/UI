7d1026644c4ea09fef14da122c61899fbe7656d7
stablecoinfactory <102005352+stablecoinfactory@users.noreply.github.com>
2022-04-04 10:59:10 -0700

    push

--- src/components/medium.vue
+++ src/components/medium.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray-100">
-    <div class="container mx-auto px-5 py-20">
+    <div class="container mx-auto max-w-7xl px-5 py-32">
       <div class="py-5">
         <p class="uppercase m-4 text-4xl text-gray-700 text-medium">
           Recent Posts
